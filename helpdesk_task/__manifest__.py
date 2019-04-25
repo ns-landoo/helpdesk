04b3ac2676b4520c071a817c47a42572a2a27154
nagore <ns@landoo.es>
2019-04-25 09:07:54 +0200

    [ADD] New module helpdesk_task to join with ticket/projects/task

---
+++ helpdesk_task/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Nagore Salaberria <ns@landoo.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# -*- coding: utf-8 -*-
+{
+    'name': "Helpdesk Task",
+    'summary': "Module Helpdesk join to Projects Task",
+    'author': "Nagore Salaberria <ns@landoo.es>",
+    'license': "AGPL-3",
+    'category': 'Uncategorized',
+    'version': "12.0.1.0.0",
+    "application": False,
+    "installable": True,
+    'depends': [
+        'project',
+        'helpdesk',
+        'helpdesk_stock'
+    ],
+    'data':[
+         'views/helpdesk_task_view.xml',
+         'security/ir.model.access.csv',
+    ],
+}
---
+++ helpdesk_task/models/helpdesk_task.py
@@ -0,0 +1,32 @@
+# Copyright 2019 Oscar Soto
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models, _
+
+
+class HelpdeskTask(models.Model):
+
+    _name = 'helpdesk.task'
+    _description = 'Helpdesk Task'
+    _inherit = 'helpdesk.ticket'
+
+
+    project_id = fields.Many2one(comodel_name="project.project",
+                                 string="project",
+                                 required=False)
+    task_id = fields.Many2one(comodel_name="project.task",
+                              string="task",
+                              required=True)
+    planned_hours = fields.Float(string='planned hours',
+                                related='task_id.planned_hours',
+                                readonly=True)
+
+    effective_hours = fields.Float(string='planned hours',
+                                related='task_id.effective_hours',
+                                readonly=True)
+
+    timesheet_ids = fields.One2many(comodel_name='project.task',
+                                    inverse_name='task_id',
+                                    string='timesheet',
+                                    related='task_id.timesheet_ids',
+                                    required=True)
